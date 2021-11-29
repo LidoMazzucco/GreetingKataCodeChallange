65ba9c5c322f62c83d5f44dce6cf3754549c8a70
Lido Mazzucco <lido.mazzucco@signavio.com>
2021-11-29 10:15:08 +0100

    requirement 5

--- src/main/java/com/signavio/solution/GreetingKata.java
+++ src/main/java/com/signavio/solution/GreetingKata.java
@@ -9,7 +9,7 @@ public class GreetingKata {
 
     public static String greet(String name) {
 
-        StringBuilder result = new StringBuilder();
+        //StringBuilder result = new StringBuilder();
 
         if (name == null || name.equals("")) return "Hello, my friend.";
         if (name.toUpperCase().equals(name)) return "HELLO " + name + "!";
@@ -19,7 +19,14 @@ public class GreetingKata {
 
 
     public static String greet(String[] names) {
+        StringBuilder result = new StringBuilder();
+        result.append("Hello");
 
-        return "Hello, " + names[0] + " and " + names[1] + ".";
+        for (int i = 0; i < names.length -1; i++) {
+            result.append(", ").append(names[i]);
+        }
+        result.append(" and ").append(names[names.length - 1]).append(".");
+
+        return result.toString();
     }
 }
